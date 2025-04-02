4805b8a24481738247ac50d2a10003131a1c88a8
Maxim Rozhkov <maks.rozhkov.2004@gmail.com>
2025-04-02 20:05:26 +0500

    feat: add auth router

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.auth.current_user import get_current_user, is_user_admin
 from app.core.db import get_async_session
 from app.crud.v1.user import user_crud
 from app.schemas.user import (
@@ -21,3 +22,15 @@ async def register_user(
 ) -> UserResponse:
     user = await user_crud.add_user(body.name, session)
     return UserResponse.model_validate(user)
+
+
+@router.get(
+    '/public/profile',
+    summary='Получение профиля пользователя',
+    tags=['public']
+)
+async def get_profile_user(
+        current_user: AsyncSession = Depends(get_current_user),
+) -> UserResponse:
+    user = UserResponse.model_validate(current_user)
+    return user
---
+++ app/core/auth/current_user.py
@@ -0,0 +1,27 @@
+from fastapi import Depends, HTTPException, Security
+from fastapi.security import APIKeyHeader
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from app.core.db import get_async_session
+from app.core.enums import UserRole
+from app.models import User
+
+auth_header = APIKeyHeader(name="Authorization", auto_error=True)
+
+
+async def get_current_user(api_key: str = Security(auth_header),
+                           session: AsyncSession = Depends(get_async_session)) -> User:
+    result = await session.execute(select(User).where(User.api_key == api_key))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid or missing API Key")
+
+    return user
+
+
+async def is_user_admin(user: User = Depends(get_current_user)) -> User:
+    if user.role != UserRole.ADMIN:
+        raise HTTPException(status_code=403, detail="Access forbidden: Admins only")
+    return user
