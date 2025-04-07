48efecfc2c77adbbe17c5f6b8c5fc8e0d9361052
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2025-04-07 23:39:36 +0500

    time enum

--- app/core/enums.py
+++ app/core/enums.py
@@ -1,6 +1,9 @@
-from enum import StrEnum, auto
+from enum import StrEnum, auto, Enum
 
 
 class UserRole(StrEnum):
     ADMIN = auto()
     USER = auto()
+
+class CurrencyTicker(str, Enum):
+    RUB = "RUB"
