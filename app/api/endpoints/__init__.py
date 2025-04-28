df033d978d647eaa4d405a9ad2de03221dff6110
Vadim <92179377+Zhuzhalica@users.noreply.github.com>
2025-04-28 20:32:13 +0500

    done 2

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -3,5 +3,6 @@
 from .health import router as health_router  # noqa: F401
 from .user import router as user_router  # noqa: F401
 from .admin.balance import router as balance_router  # noqa: F401
+from .admin.user import router as admin_user_router  # noqa: F401
 
-__all__ = ['health_router', 'user_router', 'balance_router']
+__all__ = ['health_router', 'user_router', 'balance_router', 'admin_user_router']
